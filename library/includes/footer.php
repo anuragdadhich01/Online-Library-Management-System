53d8234bcc943ef4990a350414afe15f661dc103
Anurag Dadhich <2022pietcsanurag025@poornima.org>
2024-06-05 18:33:13 +0530

    all

---
+++ library/includes/footer.php
@@ -0,0 +1,10 @@
+   <section class="footer-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                   &copy; 2024 Online Library Management System |<a href="https://github.com/anuragdadhich01" target="_blank"  > Designed by : Anurag Dadhich</a> 
+                </div>
+
+            </div>
+        </div>
+    </section>
